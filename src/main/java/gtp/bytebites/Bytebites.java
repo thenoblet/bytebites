dc826ec61f0424f0060139534c17b6ca64161393
thenoblet <appiahpatricknoblet@gmail.com>
2025-06-30 11:14:02 +0000

    Add dependencies

---
+++ src/main/java/gtp/bytebites/Bytebites.java
@@ -0,0 +1,12 @@
+package gtp.bytebites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bytebites {
+    public static void main(String[] args) {
+        SpringApplication.run(Bytebites.class, args);
+    }
+
+}
---
+++ src/test/java/gtp/bytebites/BytebitesApplicationTests.java
@@ -0,0 +1,13 @@
+package gtp.bytebites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytebitesTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
